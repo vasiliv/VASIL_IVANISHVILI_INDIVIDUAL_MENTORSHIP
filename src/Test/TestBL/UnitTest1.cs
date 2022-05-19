48360dce92c67a14c231a3a766bdd336f3763521
Vasil Ivanishvili <ggo24@yahoo.com>
2022-05-19 19:54:32 +0400

    insert FakeHttpClient method to UnitTest1 class

--- src/Test/TestBL/UnitTest1.cs
+++ src/Test/TestBL/UnitTest1.cs
@@ -1,4 +1,9 @@
 using BL;
+using Moq;
+using Moq.Protected;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -16,5 +21,25 @@ namespace TestBL
             //Assert
             //Assert.Equal(expected, actual);
         }
-    }
+        public static HttpClient FakeHttpClient(string response)
+        {
+            response ??= "{'main':{'temp':10.0}}";
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+               .Protected()
+               // Setup the PROTECTED method to mock
+               .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>()
+               )
+               // prepare the expected response of the mocked http call
+               .ReturnsAsync(new HttpResponseMessage()
+               {
+                   StatusCode = HttpStatusCode.OK,
+                   Content = new StringContent(response),
+               });
+            return new HttpClient(handlerMock.Object);
+        }
+    }    
 }
